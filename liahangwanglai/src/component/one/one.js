33cf405e1e05de213b2d04c93c6e413de7deac7c
yangjianghui <3262459828@qq.com>
2020-03-16 10:50:01 +0800

    礼尚往来

--- liahangwanglai/src/component/one/one.js
+++ liahangwanglai/src/component/one/one.js
@@ -3,48 +3,46 @@ import './one.css'
 import {NavLink} from 'react-router-dom'
 
 export default class one extends Component{
-    constructor(){
-        super();
-        this.state = {
-            tabs:[
-                {tabName:"待参加",id:1},
-                {tabName:"已参加",id:2},
-            ],
-            currentIndex:1,
-        };
-    }
-    componentDidMount() {
-        
-    }
-    tabChoiced=(id)=>{
-        // tab切换的方法
-        this.setState({
-            currentIndex:id
-        });
+    constructor(props){
+        super(props)
+        this.state={
+            navList:["tab1","tab2"],
+            content:["111111","2222222"],
+            current:0
+        }
     }
     render(){
-        var _this=this;
-        var tabList= this.state.tabs.map(function(res,index) {
-            var tabStyle=res.id==this.state.currentIndex ? 'subCtrl active' : 'subCtrl';
-            return    
-                <li key={index} onClick={this.tabChoiced.bind(_this,res.id)} className={tabStyle}>{res.tabName}</li>
-        }.bind(_this));
-        return (
+        return(
             <div style={{background:'#f2f2f2',height:'546px'}}>
                 <div className="hea">
                     <NavLink to="/" className="lll">
-                        <img src="http://www.axshare.cn/gsc/9B0J2A/6b/b2/f3/6bb2f30d8aa742729e588769883d60fd/images/礼账列表/u84.png?token=ff14f5b935ac8f26062b70423e3cb04b0b48431c30ba266bdf3e352e1993d7e2" />
+                        <img src="http://www.axshare.cn/gsc/9B0J2A/6b/b2/f3/6bb2f30d8aa742729e588769883d60fd/images/礼账列表/u84.png?token=ff14f5b935ac8f26062b70423e3cb04b0b48431c30ba266bdf3e352e1993d7e2" />                    
                     </NavLink>
                     <div className="ss">
-                        {tabList}
+                        {
+                            this.state.navList.map((item,index)=>{
+                                return <span key={index}
+                                onClick={this.currentIndex.bind(this,index)}
+                                className={this.state.current === index ? "activee":""}>{item}</span>
+                            })
+                        }
                     </div>
                     <div>
                         <img src="http://www.axshare.cn/gsc/9B0J2A/6b/b2/f3/6bb2f30d8aa742729e588769883d60fd/images/礼账列表/u90.svg?token=fe9fb97c4e04137da23a4c642d6e6761a2b86dc595a3e4cd1dbd530da68be456" />
                     </div>
                 </div>
                 <div className="leiq">
-                    <div>类别</div>
-                    <div>时间</div>
+                     <div>类别</div>
+                     <div>时间</div>
+                 </div>
+                <div>
+                    {
+                        this.state.content.map((item,index)=>{
+                            return <ul key={index} className={index === this.state.current ? "active1" : ""}>
+                                <li>{item}</li>
+                            </ul>
+                        })
+                    }
                 </div>
                 <div className="mu">
                     <div className="mu_left">
@@ -85,4 +83,59 @@ export default class one extends Component{
             </div>
         )
     }
+    currentIndex(index){
+        this.setState({
+            current:index
+        })
+    }
 }
+
+// export default class one extends Component{
+//     constructor(){
+//         super();
+//         this.state = {
+//             tabs:[
+//                 {tabName:"待参加",id:1},
+//                 {tabName:"已参加",id:2},
+//             ],
+//             currentIndex:1,
+//         };
+//     }
+//     componentDidMount() {
+        
+//     }
+//     tabChoiced=(id)=>{
+//         // tab切换的方法
+//         this.setState({
+//             currentIndex:id
+//         });
+//     }
+//     render(){
+//         var _this=this;
+//         var tabList= this.state.tabs.map(function(res,index) {
+//             var tabStyle=res.id==this.state.currentIndex ? 'subCtrl active' : 'subCtrl';
+//             return    
+//                 <li key={index} onClick={this.tabChoiced.bind(_this,res.id)} className={tabStyle}>{res.tabName}</li>
+//         }.bind(_this));
+//         return (
+//             <div style={{background:'#f2f2f2',height:'546px'}}>
+//                 <div className="hea">
+//                     <NavLink to="/" className="lll">
+//                         <img src="http://www.axshare.cn/gsc/9B0J2A/6b/b2/f3/6bb2f30d8aa742729e588769883d60fd/images/礼账列表/u84.png?token=ff14f5b935ac8f26062b70423e3cb04b0b48431c30ba266bdf3e352e1993d7e2" />
+//                     </NavLink>
+//                     <div className="ss">
+//                         {tabList}
+//                     </div>
+//                     <div>
+//                         <img src="http://www.axshare.cn/gsc/9B0J2A/6b/b2/f3/6bb2f30d8aa742729e588769883d60fd/images/礼账列表/u90.svg?token=fe9fb97c4e04137da23a4c642d6e6761a2b86dc595a3e4cd1dbd530da68be456" />
+//                     </div>
+//                 </div>
+//                 <div className="leiq">
+//                     <div>类别</div>
+//                     <div>时间</div>
+//                 </div>
+                
+//             </div>
+//         )
+//     }
+// }
